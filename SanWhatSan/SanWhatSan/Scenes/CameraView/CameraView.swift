42404147c71b3cfb9ea9ef9aff317114df322421
MINJEONG <160840239+whalswjd@users.noreply.github.com>
2025-07-07 19:14:33 +0900

    chore: modify file directory
    add scenes folder

---
+++ SanWhatSan/SanWhatSan/Scenes/CameraView/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraView.swift
+//  SanWhatSan
+//
+//  Created by Zhen on 7/7/25.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
