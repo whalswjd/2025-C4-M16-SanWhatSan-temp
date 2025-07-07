08491c99a2c9456e0127ecb1a90098d5523382cb
MINJEONG <160840239+whalswjd@users.noreply.github.com>
2025-07-07 17:03:55 +0900

    chore: add XcodeFile
    SanWhatSan 파일 추가했습니다

---
+++ SanWhatSan/SanWhatSan/App/SanWhatSanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanWhatSanApp.swift
+//  SanWhatSan
+//
+//  Created by Zhen on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SanWhatSanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
