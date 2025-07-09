a57636f4d25a1c2cbc1f9be5b09f94c51ca9dcfd
simi-sumin <sudesign272@gmail.com>
2025-07-09 10:49:12 +0900

    이미지뷰작업완

---
+++ SanWhatSan/Scenes/ImageView/ShareSheet.swift
@@ -0,0 +1,18 @@
+//
+//  ShareSheet.swift
+//  SanWhatSan
+//
+//  Created by 장수민 on 7/9/25.
+//
+
+import SwiftUI
+
+struct ShareSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareSheet()
+}
