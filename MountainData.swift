32bc66d088acbd5ba707741f75696e4640d19a6d
Junia-Choi <Junia77yeen@gmail.com>
2025-07-21 09:48:36 +0900

    first commit

---
+++ MountainData.swift
@@ -0,0 +1,18 @@
+//
+//  MountainData.swift
+//  SanWhatSan
+//
+//  Created by 최예은 on 7/18/25.
+//
+
+import SwiftUI
+
+struct MountainData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MountainData()
+}
